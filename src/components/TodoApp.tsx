1e3697492d481f0b52227ab4779f5b222fcb055c
LaurentLallement <lallela95@gmail.com>
2025-03-16 21:08:57 +0100

    ajout de la gestion des evenements sur le composant TodoItem

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -8,10 +8,12 @@ export function TodoApp() {
 
     // Gestion de cocher/décocher
     const handleToggle = (id: number): void => {
+        console.log("Tâche cochée/décochée avec l'ID :", id);
     };
 
     // Gestion de la suppression
     const handleDelete = (id: number): void => {
+        console.log("Tâche supprimée avec l'ID :", id);
     };
 
     return (
@@ -21,23 +23,21 @@ export function TodoApp() {
                 <TodoItem
                     key={1}
                     task={{id:1, text:"Acheter des oranges", isCompleted:false}}
-                    onToggle={handleToggle}
-                    onDelete={handleDelete}
-                />
-
+                    onToggle={() => handleToggle(1)}
+                    onDelete={() => handleDelete(1)}                />
                 <TodoItem
                     key={2}
                     task={{id:2, text:"Courir avec le fraté", isCompleted:true}}
-                    onToggle={handleToggle}
-                    onDelete={handleDelete}
-                />
+
+                    onToggle={() => handleToggle(2)}
+                    onDelete={() => handleDelete(2)}                />
 
                 <TodoItem
                     key={3}
                     task={{id:3, text:"Me faire défoncer à LoL", isCompleted:true}}
-                    onToggle={handleToggle}
-                    onDelete={handleDelete}
-                />
+
+                    onToggle={() => handleToggle(3)}
+                    onDelete={() => handleDelete(3)}                />
             </div>
         </>
     );
